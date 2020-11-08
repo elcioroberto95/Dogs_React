2ff2bc33673fe9e7315d0900672fc917f1f57136
Elcio <elcioroberto1995@gmail.com>
2020-11-08 19:04:37 -0300

    Routas do login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import './App.css';
 const App = () => {
   return (
     <div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
+import { ReactComponent as Dogs } from '../Assets/dogs.svg';
 const Header = () => {
   return (
     <div className={styles.header}>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login/Criar Login</Link>
+      <nav className={`${styles.nav} container`}>
+        <Link styles={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login/Criar Login
+        </Link>
       </nav>
     </div>
   );
