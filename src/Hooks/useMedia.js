0d891c421a8a4454776f1486ffee00d066616d55
Elcio <elcioroberto1995@gmail.com>
2020-11-13 14:34:40 -0300

    postagem de fotos

--- src/Hooks/useMedia.js
+++ src/Hooks/useMedia.js
@@ -9,6 +9,7 @@ const useMedia = (media) => {
       setMatch(matches);
     }
     changeMatch();
+
     window.addEventListener('resize', changeMatch);
     return () => {
       window.removeEventListener('resize', changeMatch);
