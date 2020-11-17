d3c8f5df7c5fe2ded511dfa60f8115c9c24ec100
Elcio <elcioroberto1995@gmail.com>
2020-11-17 14:26:51 -0300

    pagina de estatisticas

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { STATS_GET } from '../../Api';
+import Error from '../../Helper/Error';
 import Head from '../../Helper/Head';
+import Loading from '../../Helper/Loading';
+import useFetch from '../../Hooks/useFetch';
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
-  <Head title="Estatisticas" />;
-  return <div>Estatisticas</div>;
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatisticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
